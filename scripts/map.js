4640cd1f85ae627ba21305aef4885bc0107f25d1
Matthew Nerger <mnerger@lsu.edu>
2016-04-10 22:36:35 -0500

    Update app

--- scripts/map.js
+++ scripts/map.js
@@ -1,8 +1,20 @@
+'use strict';
+
 function initMap() {
     // Create a map object and specify the DOM element for display.
     var map = new google.maps.Map(document.getElementById('map-div'), {
         center: {lat: 27.865, lng: -82.459},
         mapTypeControl: false,
-        zoom: 9
+        zoom: 10
+    });
+
+    // Iterate through locationArray and add markers to the map
+    locationArray.forEach(function(place) {
+        var marker = new google.maps.Marker({
+            position: {lat: place.lat, lng: place.lng},
+            map: map,
+            title: place.name,
+            label: place.mapLabel
+        });
     });
 }
--- scripts/viewmodel.js
+++ scripts/viewmodel.js
@@ -1,33 +1,68 @@
+'use strict';
+
 var locationArray = [
     {
-        name: 'Tampa',
+        name: 'Books-a-Million',
         desc: 'null',
-        lat: null,
-        long: null
+        mapLabel: 'S',
+        lat: 27.93233,
+        lng: -82.32699
     },
     {
-        name: 'Tampa',
+        name: 'The Center of the Universe',
         desc: 'null',
-        lat: null,
-        long: null
+        mapLabel: 'S',
+        lat: 27.84804,
+        lng: -82.27686
     },
     {
-        name: 'Tampa',
+        name: "Haslam's",
         desc: 'null',
-        lat: null,
-        long: null
+        mapLabel: 'R',
+        lat: 27.77132,
+        lng: -82.66082
     },
     {
-        name: 'Tampa',
+        name: 'The Old House',
         desc: 'null',
-        lat: null,
-        long: null
+        mapLabel: 'R',
+        lat: 27.7624,
+        lng: -82.73724
     },
     {
-        name: 'Tampa',
+        name: 'Park Square',
         desc: 'null',
-        lat: null,
-        long: null
+        mapLabel: '0',
+        lat: 27.854269,
+        lng: -82.210625
+    },
+    {
+        name: 'Paul Sanders Park',
+        desc: 'null',
+        mapLabel: 'S',
+        lat: 27.89425,
+        lng: -82.29326
+    },
+    {
+        name: 'Railroad Tracks',
+        desc: 'null',
+        mapLabel: 'S',
+        lat: 27.94627,
+        lng: -82.29975
+    },
+    {
+        name: 'Tampa Convention Center',
+        desc: 'null',
+        mapLabel: 'S',
+        lat: 27.9419,
+        lng: -82.45652
+    },
+    {
+        name: 'Winthrop Village',
+        desc: 'null',
+        mapLabel: 'S',
+        lat: 27.893256,
+        lng: -82.316763
     }
 ];
 
