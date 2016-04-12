98f879f1917aad70eb4c672a1a1ebfde38a7ede4
Matthew Nerger <mnerger@lsu.edu>
2016-04-12 17:51:08 -0500

    Update site assets

--- scripts/map.js
+++ scripts/map.js
@@ -1,8 +1,10 @@
 'use strict';
 
+var map = 0;
+
 function initMap() {
     // Create a map object and specify the DOM element for display.
-    var map = new google.maps.Map(document.getElementById('map-div'), {
+    map = new google.maps.Map(document.getElementById('map-div'), {
         center: {lat: 27.865, lng: -82.459},
         mapTypeControl: false,
         zoom: 10
@@ -15,6 +17,11 @@ function initMap() {
             map: map,
             title: place.name,
             label: place.mapLabel
+        });
+
+        marker.addListener('click', function() {
+            map.setZoom(13);
+            map.panTo(marker.getPosition());
         });
     });
 }
