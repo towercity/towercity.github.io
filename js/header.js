1b43b78002974481af56b8f29ebaf740bfc7c3ab
towercity <matthewjnerger3@gmail.com>
2016-10-09 15:38:26 -0400

    Move header to a header-rendering function for portability

---
+++ js/header.js
@@ -0,0 +1,45 @@
+var headerView = {
+  'html': {
+    'start': '<header><div class="header-links"><ul>',
+    'link': '<li><a href="%url%" alt="%alt%">%title%</a></li>',
+    'end': '</ul></div></header>'
+  },
+  'links': [
+    {
+      'title': 'home',
+      'url': 'index.html',
+      'alt': 'return home'
+    },
+    {
+      'title': 'bio',
+      'url': 'bio.html',
+      'alt': 'biography page'
+    },
+    {
+      'title': 'code',
+      'url': 'code.html',
+      'alt': 'code samples'
+    },
+    {
+      'title': 'prose',
+      'url': 'prose.html',
+      'alt': 'writing samples'
+    },
+    {
+      'title': 'contact',
+      'url': 'contact.html',
+      'alt': 'contact me'
+    }
+  ],
+  render: function() {
+    var printHTML = this.html.start;
+    this.links.forEach(function(link) {
+      printHTML += headerView.html.link.replace("%url%", link.url).replace("%alt%", link.alt).replace("%title%", link.title);
+    });
+    printHTML += this.html.end;
+
+    $('body').append(printHTML);
+  }
+};
+
+headerView.render();
