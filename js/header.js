a9ea4fb97f0574990db6aa36b40631b1cdca2432
towercity <matthewjnerger3@gmail.com>
2016-10-23 23:11:35 -0400

    Alter headerlinks display

--- js/header.js
+++ js/header.js
@@ -1,63 +1,84 @@
 var headerView = {
   'html': {
-    'start': '<header><div class="header-img"><div class="header-links"><ul>',
-    'link': '<li><a href="%url%" alt="%alt%" %blank%>%title%</a></li>',
-    'end': '</ul></div></div><div class="header-color"></div></header>'
+    'start': '<header><div class="header-img"></div>',
+    'logo': '<div class="logo"><a href="index.html"><img src="%img%" alt="%alt%"></a></div>',
+    'middle': '<nav class="header-links"><ul>',
+    'link': '<li><a href="%url%" alt="%alt%" %blank% class="%class%">%title%</a></li>',
+    'end': '</ul></nav><div class="header-color"></div></header>'
   },
-  'links': [
-    {
-      'title': 'home',
-      'url': 'index.html',
-      'alt': 'return home'
-    },
-    {
-      'title': 'bio',
-      'url': 'bio.html',
-      'alt': 'biography page'
-    },
-    {
-      'title': 'code',
-      'url': 'code.html',
-      'alt': 'code samples'
-    },
-    {
-      'title': 'github',
-      'url': 'https://github.com/towercity',
-      'alt': 'github profile',
-      'newTab': true
-    },
-    {
-      'title': 'contact',
-      'url': 'mailto:matthewjnerger3@gmail.com',
-      'alt': 'contact me'
-    }
-  ],
+  'logo': {
+    'img': '',
+    'alt': ''
+  },
+  'links': {
+    'left': [
+      {
+        'title': 'bio',
+        'url': 'bio.html',
+        'alt': 'biography page'
+      },
+      {
+        'title': 'code',
+        'url': 'code.html',
+        'alt': 'code samples'
+      }
+    ],
+    'right': [
+      {
+        'title': 'github',
+        'url': 'https://github.com/towercity',
+        'alt': 'github profile',
+        'newTab': true
+      },
+      {
+        'title': 'contact',
+        'url': 'mailto:matthewjnerger3@gmail.com',
+        'alt': 'contact me'
+      }
+    ]
+  },
+
   render: function() {
     var printHTML = this.html.start;
-    this.links.forEach(function(link) {
-      var linkHTML = headerView.html.link.replace("%url%", link.url).replace("%alt%", link.alt);
 
-      //Checks for current page
-      var href = document.location.href;
-      if (href.substr(href.lastIndexOf('/') + 1) === link.url) {
-        linkHTML = linkHTML.replace("%title%", ("> " + link.title));
-      } else {
-        linkHTML = linkHTML.replace("%title%", link.title);
-      }
+    printHTML += this.html.logo.replace("%img%", this.logo.img).replace("%alt%", this.logo.alt);
+    printHTML += this.html.middle;
 
-      //Check for target_blank
-      if (link.newTab) {
-        linkHTML = linkHTML.replace("%blank%", 'target="_blank"');
-      } else {
-        linkHTML = linkHTML.replace("%blank%", '');
-      }
-
-      console.log(linkHTML);
-      printHTML += linkHTML;
+    this.links.left.forEach(function(link) {
+      printHTML += headerView.htmlFromLinks(link, 'float-left');
     });
+
+    this.links.right.forEach(function(link) {
+      printHTML += headerView.htmlFromLinks(link, 'float-right');
+    });
+
     printHTML += this.html.end;
 
     $('body').prepend(printHTML);
+  },
+
+  htmlFromLinks: function(link, addClass) {
+    var linkHTML = headerView.html.link.replace("%url%", link.url).replace("%alt%", link.alt);
+
+    //Checks for current page
+    var href = document.location.href;
+    if (href.substr(href.lastIndexOf('/') + 1) === link.url) {
+      linkHTML = linkHTML.replace("%title%", link.title);
+    } else {
+      linkHTML = linkHTML.replace("%title%", link.title);
+    }
+
+    //Check for target_blank
+    if (link.newTab) {
+      linkHTML = linkHTML.replace("%blank%", 'target="_blank"');
+    } else {
+      linkHTML = linkHTML.replace("%blank%", '');
+    }
+
+    //add float class
+    linkHTML = linkHTML.replace("%class%", addClass);
+
+    return linkHTML;
   }
 };
 
